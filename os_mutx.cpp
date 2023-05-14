1e5145731e072b28a05c0e247ff2d17933c95829
William Redenbaugh <wmredenbaugh@gmail.com>
2023-05-14 14:36:08 -0700

    csal stuff

---
+++ os_mutx.cpp
@@ -0,0 +1,48 @@
+#include "os_mutx.h"
+#include "os_error.h"
+
+int os_mut_init(os_mut_t *mut){
+    if(mut == NULL){
+        return OS_RET_NULL_PTR;
+    }
+
+    mut->lock = Mutex();
+
+    return OS_RET_OK;
+}
+
+int os_mut_entry(os_mut_t *mut, uint32_t timeout_ms){
+    if(mut == NULL){
+        return OS_RET_NULL_PTR;
+    }
+    mut->lock.try_lock()
+
+    return OS_RET_OK;
+}
+
+int os_mut_count(os_mut_t *mut){
+    if(mut == NULL){
+        return OS_RET_NULL_PTR;
+    }
+    return OS_RET_OK;
+}
+
+int os_mut_entry_wait_indefinite(os_mut_t *mut){
+    if(mut == NULL){
+        return OS_RET_NULL_PTR;
+    }
+
+    mut->lock.lock();
+    return OS_RET_OK;
+}
+
+int os_mut_exit(os_mut_t *mut){
+    if(mut == NULL){
+        return OS_RET_NULL_PTR;
+    }
+    mut->lock.unlock();
+
+    return OS_RET_OK;
+}
+
+#endif
---
+++ os_thread.cpp
@@ -0,0 +1,10 @@
+#include "os_thread.h"
+#include "Particle.h"
+
+// Function to create a new thread
+os_thread_id_t os_add_thread(thread_func_t p, void * arg, int stack_size, void *stack){
+  os_thread_id_t thread_id;
+  thread_id.handle = new Thread("", p, 0, stack_size);
+
+  return thread_id;
+}
