8e0b626b65766cb2af85c687f799cce4c4861fdf
William Redenbaugh <wmredenbaugh@gmail.com>
2023-05-26 19:30:28 -0700

    eventmanagement and queue implementation for Particle CSAL

---
+++ event_management.cpp
@@ -0,0 +1,21 @@
+#include "event_management.h"
+
+void event_management_init(void *params){
+
+}
+
+void event_management_thread(void *parameters){
+
+}
+
+int subscribe_eventlist(int *event_list, int num_events, int event_length_max){
+
+}
+
+bool available_events(int eventspace){
+
+}
+
+event_data_t consume_event(int eventspace){
+
+}
