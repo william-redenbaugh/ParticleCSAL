03224c37fdcb6ac0533692a6454497f0f57bb8d6
William Redenbaugh <wmredenbaugh@gmail.com>
2023-05-29 22:01:25 -0700

    misc header, subrepo update

---
+++ os_misc.cpp
@@ -0,0 +1,6 @@
+#include "os_misc.h"
+#include "Particle.h"
+
+void println(char *str){
+    Log.info(str);    
+}
---
+++ os_misc.h
@@ -0,0 +1,7 @@
+#ifndef _OS_MISC_H
+#define _OS_MISC_H
+
+#include "Particle.h"
+void println(char *str);
+
+#endif
